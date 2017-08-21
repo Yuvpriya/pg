169542f1e53a8bdc9248e5f094b100a754dcc653
Jazz <Yuvpriya@users.noreply.github.com>
2017-08-21 13:54:35 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,21 @@
+include <stdio.h>
+int main()
+{
+	int n,rev=0,rem;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		rem=n%10;
+		rev=(rev*10)+rem;
+		n=n/10;
+	}
+	
+	if(n==rev){
+		printf("Palindrome");
+		}
+		else
+		{
+			printf("Not a palindrome");
+		}
+	return 0;
+}
