f59041043363439defd0ed968f35a1af70a78f4c
Jazz <Yuvpriya@users.noreply.github.com>
2017-08-21 14:04:50 +0530

    Create largee.c

---
+++ largee.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("A is Largest");
+else if((b>c))
+printf("B is Largest");
+else
+printf("C is Largest");
+getch();
+}
