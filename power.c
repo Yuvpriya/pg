7e98e60dfab892837ccbc4e75ec74061485a7a46
Jazz <Yuvpriya@users.noreply.github.com>
2017-08-21 13:49:08 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int p,q;
+    long long int r=1;
+    scanf("%d",&p);
+    scanf("%d",&q);
+    while(q!= 0)
+    {
+        r*=p;
+        q--;
+    }
+
+    printf("%lld",c);
+
+    return 0;
+}
