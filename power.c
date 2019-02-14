4bba537f1a5a6a1f03425161d608f4c4e7531e6a
Jazz <Yuvpriya@users.noreply.github.com>
2019-02-14 13:47:14 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int p,q;
-    long long int r=1;
-    scanf("%d",&p);
-    scanf("%d",&q);
-    while(q!= 0)
-    {
-        r*=p;
-        q--;
-    }
-
-    printf("%lld",c);
-
-    return 0;
+	int n,rev=0,rem,y;
+	scanf("%d",&n);
+	y=n;
+	while(n>0)
+	{
+		rem=n%10;
+		rev=(rev*10)+rem;
+		n=n/10;
+	}
+	if(y==rev)
+	{
+		printf("\nPalindrome");
+		}
+		else
+			printf("\nNot a palindrome");
+	return 0;
 }
